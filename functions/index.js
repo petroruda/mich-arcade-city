54fc954d0aeee575a002d89fcc41d4097a4b5803
petroruda <petro.ruda001@yandex.com>
2017-06-16 00:40:30 +0200

    Building User Profile

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,24 @@
-var functions = require('firebase-functions');
+const functions = require('firebase-functions');
+const admin = requir('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Start writing Firebase Functions
+const ref = admin.database().ref();
+
+
+
+// // Create User Ref when new user is created;
 // // https://firebase.google.com/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// })
+
+ exports.createUserAccount = functions.auth.user().onCreate(event => {
+    const uid = event.data.uid;
+    const email = event.data.email;
+    const photo = event.data.photoURL;
+    const name = event.data.displayName || "https://ac-app-2a741.firebaseapp.com/images/user.jpg";
+
+    const newUserRef = ref.child('users/${uid}');
+    return newUserRef.set({
+        photoURL: photo,
+        email: email,
+        name: name,
+    })
+})
